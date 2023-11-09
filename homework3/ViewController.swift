7af3e70eebbdd08d146fb0308c03a5f9361b6809
poftnat <149266332+poftnat@users.noreply.github.com>
2023-11-09 22:30:19 +0300

    done

--- homework3/ViewController.swift
+++ homework3/ViewController.swift
@@ -9,6 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumberTextfield: UITextField!
+    @IBOutlet weak var secondNumberTextfield: UITextField!
+    @IBOutlet weak var operationTextfield: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        if let firstNumberText = firstNumberTextfield.text, let firstNumber = Int(firstNumberText) {
+            if let secondNumberText = secondNumberTextfield.text, let secondNumber = Int(secondNumberText) {
+                switch operationTextfield.text {
+                case "+" :
+                    resultLabel.text = String(firstNumber + secondNumber)
+                case "-" :
+                    resultLabel.text = String(firstNumber - secondNumber)
+                case "*" :
+                    resultLabel.text = String(firstNumber * secondNumber)
+                case "/" :
+                    resultLabel.text = String(Double(firstNumber) / Double(secondNumber))
+                default :
+                    resultLabel.text = "Некорректные данные"
+                }
+                
+            } else {
+                resultLabel.text = "Некорректные данные"
+            }
+            
+        } else {
+            resultLabel.text = "Некорректные данные"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
